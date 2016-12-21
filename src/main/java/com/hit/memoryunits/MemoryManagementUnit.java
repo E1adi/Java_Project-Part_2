5966bcb473737eb0ed3a93c8d27efad5b1a4760c
E1adi <isrelad92@gmail.com>
2016-12-21 15:35:52 +0200

    Part 2 - Init

---
+++ src/main/java/com/hit/memoryunits/MemoryManagementUnit.java
@@ -0,0 +1,23 @@
+package com.hit.memoryunits;
+
+import com.hit.algorithm.IAlgoCache;
+
+public class MemoryManagementUnit {
+	
+//	IAlgoCache<java.lang.Long,java.lang.Long> algo;
+//	
+//	public MemoryManagementUnit(int ramCapacity,
+//            com.hit.algorithm.IAlgoCache<java.lang.Long,java.lang.Long> algo) {
+//		
+//	}
+//	
+////	This method is the main method which returns array of pages that are requested from the user
+////	Parameters:
+////		pageIds - array of page id's
+////	Returns:
+////		returns array of pages that are requested from the user
+////	Throws:
+////		java.io.IOException
+//	public Page<byte[]>[] getPages(java.lang.Long[] pageIds)
+//            throws java.io.IOException
+}
---
+++ src/main/java/com/hit/memoryunits/Page.java
@@ -0,0 +1,41 @@
+package com.hit.memoryunits;
+
+public class Page<T> implements java.io.Serializable {
+
+	public Page(java.lang.Long id,
+            T content) {
+		
+	}
+	
+	public java.lang.Long getPageId() {
+		return null;		
+	}
+	
+	public void setPageId(java.lang.Long pageId) {
+		
+	}
+	
+	public T getContent() {
+		return null;
+		
+	}
+	
+	public void setContent(T content) {
+		
+	}
+	
+	public int hashCode() {
+		return 0;
+		
+	}
+	
+	public boolean equals(java.lang.Object obj) {
+		return false;
+		
+	}
+	
+	public java.lang.String toString() {
+		return null;
+		
+	}
+}
